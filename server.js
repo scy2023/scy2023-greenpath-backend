e38a0d2999a56824854986d336ded7eaaa434d86
Suresh <scy981779@gmail.com>
2026-04-17 12:45:55 +0100

    add home route

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => res.send("GreenPath API is running!"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/cv", cvRoutes);
