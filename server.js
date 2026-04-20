5427263aeda2720961ce4c2c2a29712b78ade8d5
Suresh <scy981779@gmail.com>
2026-04-20 18:41:59 +0100

    add health check route

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => res.send("GreenPath API is running!"));
-
+app.get("/health", (req, res) => res.status(200).json({ status:"alive"}));
 app.use("/api/auth", authRoutes);
 app.use("/api/cv", cvRoutes);
 app.use("/api/analyze", analyzeRoutes);
